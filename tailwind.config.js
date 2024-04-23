91a4d48149a3049da7dc880b91ceb4286278f55c
José Erivam <erivamdev@gmail.com>
2024-04-23 20:13:59 -0300

    :heavy_plus_sign: build: add dependencies: Tailwind, and some files.

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'backg': '#fffdf7',
+      'black': '#212121',
+      'text': '#4f4f4f',
+      'text-out': '#90908e',
+      'primary': '#ffbf00',
+      'secundary': '#fff2cc',
+      'white': '#ffffff',
+      'red': '#e74c3c',
+      'green': '#2ecc71',
+      'separate': '#cccccc',
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
